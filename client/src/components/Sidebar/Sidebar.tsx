415cdb339cff13462c3115cfcee2ee39eebcf2f1
Jonathan Dump <jbrodii77@gmail.com>
2023-09-14 16:52:10 +0300

    Prevent sidebar from scrolling when menu is open

--- client/src/components/Sidebar/Sidebar.tsx
+++ client/src/components/Sidebar/Sidebar.tsx
@@ -37,45 +37,46 @@ export default function Sidebar() {
     setMenuVisible(!menuVisible);
   };
   return (
-    <div className={cl.sidebar}>
-      <div className={cl.header}>
-        <div className={burgerClass} onClick={handleBurgerClick}>
-          <img src={burger} alt="" />
+    <>
+      <div className={cl.sidebar}>
+        <div className={cl.header}>
+          <div className={burgerClass} onClick={handleBurgerClick}>
+            <img src={burger} alt="" />
+          </div>
+          <div className={cl.title}>50gram</div>
         </div>
-        <div className={cl.title}>50gram</div>
-      </div>
-      <div className={cl.userList}>
-        {loading ? (
-          <div>Loading...</div>
-        ) : !users.length ? (
-          <div className={cl.text}>No people yet</div>
-        ) : (
-          users
-            .sort((a, b) => {
-              if (isOnline(usersOnline, a) && !isOnline(usersOnline, b)) {
-                return -1;
-              } else if (
-                isOnline(usersOnline, b) &&
-                !isOnline(usersOnline, a)
-              ) {
-                return 1;
-              } else {
-                return 0;
-              }
-            })
-            .map((user) => {
-              return (
-                <NavLink to={`/${user._id}`} key={user._id}>
-                  <UserCard
-                    user={user}
-                    isOnline={isOnline(usersOnline, user)}
-                  />
-                </NavLink>
-              );
-            })
-        )}
-      </div>
-      {/* {menuVisible && (
+        <div className={cl.userList}>
+          {loading ? (
+            <div>Loading...</div>
+          ) : !users.length ? (
+            <div className={cl.text}>No people yet</div>
+          ) : (
+            users
+              .sort((a, b) => {
+                if (isOnline(usersOnline, a) && !isOnline(usersOnline, b)) {
+                  return -1;
+                } else if (
+                  isOnline(usersOnline, b) &&
+                  !isOnline(usersOnline, a)
+                ) {
+                  return 1;
+                } else {
+                  return 0;
+                }
+              })
+              .map((user) => {
+                return (
+                  <NavLink to={`/${user._id}`} key={user._id}>
+                    <UserCard
+                      user={user}
+                      isOnline={isOnline(usersOnline, user)}
+                    />
+                  </NavLink>
+                );
+              })
+          )}
+        </div>
+        {/* {menuVisible && (
         <div className={cl.menu}>
           <UserCard editOn={true} />
           <button type="button" onClick={handleLogOutClick}>
@@ -88,7 +89,7 @@ export default function Sidebar() {
           ></div>
         </div>
       )} */}
-
+      </div>
       <div className={menuClass}>
         <UserCard editOn={true} menuVisible={menuVisible} />
         <button type="button" onClick={handleLogOutClick}>
@@ -100,6 +101,6 @@ export default function Sidebar() {
         ref={menuBgRef}
         onClick={handleMenuBgRefClick}
       ></div>
-    </div>
+    </>
   );
 }

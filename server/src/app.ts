41dd567dc482524dab1352cc2b49b31b0a6bdd6e
Jonathan Dump <jbrodii77@gmail.com>
2023-09-21 14:10:00 +0300

    Fix

--- server/src/app.ts
+++ server/src/app.ts
@@ -16,13 +16,13 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: ["http://localhost:5173", envReader("CORS_ORIGIN")],
+    origin: "http://localhost:5173",
   },
 });
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", envReader("CORS_ORIGIN")],
+    origin: "http://localhost:5173",
   })
 );
 const port = process.env.PORT || 3000;
